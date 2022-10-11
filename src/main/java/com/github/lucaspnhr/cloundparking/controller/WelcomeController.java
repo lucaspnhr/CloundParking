3c0d46684a5ef610768f6653509bd0bcd7a39809
lucaspnhr <lucas_748l@hotmail.com>
2022-10-11 08:07:38 -0300

    changing request mapping from main controller

--- src/main/java/com/github/lucaspnhr/cloundparking/controller/WelcomeController.java
+++ src/main/java/com/github/lucaspnhr/cloundparking/controller/WelcomeController.java
@@ -1,9 +1,11 @@
 package com.github.lucaspnhr.cloundparking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class WelcomeController {
 
     @GetMapping
