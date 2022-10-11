214955d4a7ab6ec37e0625fe8c85c6880a34081e
lucaspnhr <lucas_748l@hotmail.com>
2022-10-11 07:38:38 -0300

    Initial commit

---
+++ src/main/java/com/github/lucaspnhr/cloundparking/CloundParkingApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucaspnhr.cloundparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloundParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloundParkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/lucaspnhr/cloundparking/CloundParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucaspnhr.cloundparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloundParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
