93b6784418ff4b4d5bfed76fc8f619e160e81764
lucaspnhr <lucas_748l@hotmail.com>
2022-10-11 07:51:56 -0300

    welcome controller

---
+++ src/main/java/com/github/lucaspnhr/cloundparking/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.github.lucaspnhr.cloundparking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to his new application";
+    }
+}
